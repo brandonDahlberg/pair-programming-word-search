5c13d240778b1c64d8a045006cc05c9fa52e242c
Brandon <brandon.dahlberg21@gmail.com>
2022-01-20 04:11:07 +0000

    pair programming assignement

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,44 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+//PAIR PROGRAMMING ASSIGNMENT
+// AUTHORS: @brandonDahlberg, @ThomasNgo
 
-module.exports = wordSearch
+const word1 = 'HELLO';
+
+const twoDArr = [
+	['H', 'H', 'X', 'W', 'U'],
+	['E', 'Q', 'C', 'Z', 'J'],
+	['L', 'Z', 'B', 'L', 'O'],
+	['L', 'W', 'A', 'L', 'K'],
+	['O', 'W', 'A', 'L', 'K'],
+];
+
+const wordSearch = (letters, word) => {
+	let output = [];
+	const rowsLetters = letters.length;
+	const colsLetters = letters[0].length;
+	const horizontalJoin = letters.map(ls => ls.join(''));
+	// look for word horizonatally
+
+	for (let l of horizontalJoin) {
+		if (l.includes(word)) return true;
+	}
+	// transpose the matrix
+	// look horizontally again
+
+	for (let i = 0; i < colsLetters; i++) {
+		output[i] = [];
+		for (let j = 0; j < rowsLetters; j++) {
+			output[i][j] = letters[j][i];
+			// console.log(letters[j][i]);
+		}
+	}
+	console.log(output);
+	const verticalJoin = output.map(ls => ls.join(''));
+	for (let l of verticalJoin) {
+		if (l.includes(word)) return true;
+	}
+	return false;
+};
+
+wordSearch(twoDArr, word1);
+
+module.exports = wordSearch;
